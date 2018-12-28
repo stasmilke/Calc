07925bac9deab16245eb5f764cd8b21b0e813f42
Stas Milke <svmilke@yandex.ru>
2018-12-28 20:55:57 +0300

    Bug fixes

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -10,19 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var ostatok: UILabel!
+    @IBOutlet weak var fNum: UILabel!
     @IBOutlet weak var action: UILabel!
     @IBOutlet weak var result: UILabel!
     @IBOutlet weak var clrbutton: UIButton!
-    var numberFromScreen:Double = 0
-    var firstNum:Double = 0
+    var numberFromScreen:Float = 0
+    var firstNum:Float = 0
     var lastAction:Int = 0
     var strMBClrd = true
     var actSec = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle{
@@ -30,7 +29,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func digits(_ sender: UIButton) {
-        if result.text!.count < 8  {
+        if result.text!.count < 10 || strMBClrd{
             if result.text == "-" && sender.tag == 0{
                 result.text = "0"
                 strMBClrd = true
@@ -44,18 +43,20 @@ class ViewController: UIViewController {
                     clrbutton.setTitle("C", for: .normal)
                     strMBClrd = false
                 }
+                if !action.text!.isEmpty{
+                    fNum.text = String(firstNum)
+                }
             }
-            numberFromScreen = Double(result.text!)!
+            numberFromScreen = Float(result.text!)!
+            actSec = false
         }
     }
     
     @IBAction func buttons(_ sender: UIButton) {
         action.text = sender.title(for: .normal)
-        if actSec && sender.tag == lastAction{
-            calculating(act: sender.tag)
-        }
-        else if firstNum == 0 {
+        if firstNum == 0 || !actSec{
             firstNum = numberFromScreen
+            fNum.text = String(firstNum)
         }
         actSec = true
         lastAction = sender.tag
@@ -74,11 +75,11 @@ class ViewController: UIViewController {
         if result.text == "-"{
             result.text = "0"
             strMBClrd = true
-            numberFromScreen = Double(result.text!)!
+            numberFromScreen = Float(result.text!)!
         }
         else if result.text!.hasPrefix("-"){
             result.text!.removeFirst()
-            numberFromScreen = Double(result.text!)!
+            numberFromScreen = Float(result.text!)!
         }
         else if strMBClrd && result.text == "0"{
             result.text = "-"
@@ -86,16 +87,24 @@ class ViewController: UIViewController {
         }
         else{
             result.text = "-" + result.text!
-            numberFromScreen = Double(result.text!)!
+            numberFromScreen = Float(result.text!)!
         }
     }
     
+    @IBAction func putADot(_ sender: UIButton) {
+        if !result.text!.hasSuffix("."){
+            result.text! += "."
+            numberFromScreen = Float(result.text!)!
+            strMBClrd = false
+            actSec = false
+        }
+    }
     
     @IBAction func clearing(_ sender: UIButton) {
         firstNum = 0
+        fNum.text = ""
         lastAction = 0
         action.text = ""
-        ostatok.text = ""
         clrbutton.setTitle("AC", for: .normal)
         result.text = "0"
         numberFromScreen = 0
@@ -104,7 +113,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func oneCharClr(_ sender: UIButton) {
-        if result.text!.count > 1 {
+        if result.text!.count > 1 && !result.text!.contains("e") && !result.text!.contains("ч"){
             result.text!.removeLast()
         }
         else {
@@ -112,9 +121,9 @@ class ViewController: UIViewController {
             strMBClrd = true
         }
         if result.text != "-"{
-            numberFromScreen = Double(result.text!)!
+            numberFromScreen = Float(result.text!)!
         }
-        ostatok.text = ""
+        actSec = false
     }
     
     func calculating( act: Int){
@@ -134,15 +143,26 @@ class ViewController: UIViewController {
         default:
             break
         }
-        if (firstNum >= 100000000) || (firstNum <= -10000000) {
-            result.text = "Ошибка"
-            action.text = ""
-            ostatok.text = "Нажмите С"
-            firstNum = 100000000
+        
+        if (firstNum >= 10000000000) || (firstNum <= -1000000000) {
+            result.text = String(firstNum)
         }
         else {
-            result.text = String(Int(firstNum))
-            ostatok.text = "+ " + String(firstNum - firstNum.rounded(.towardZero))
+            if firstNum.truncatingRemainder(dividingBy: 1) == 0{
+                result.text = String(Int(firstNum))
+            }
+            else {
+                result.text = String(firstNum)
+            }
         }
+        switch result.text {
+        case "nan", "inf":
+            result.text = "Не число"
+            break
+        default:
+            break
+        }
+        fNum.text = ""
+        numberFromScreen = Float(result.text!)!
     }
 }
